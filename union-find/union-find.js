854f2ccffbceb96de7d5ffb395236eb17f43959d
Kanstantsin_Vaitovich <Kanstantsin_Vaitovich@epam.com>
2015-10-27 15:27:14 +0300

    Slow implementation of Union Find

--- union-find/union-find.js
+++ union-find/union-find.js
@@ -1,11 +1,41 @@
-(function(scope){
-	'use strict';
+(function(scope) {
+    'use strict';
 
-	scope.UnionFind = function (elementsArray) {
-		this.elements = elementsArray;	
-	};
+    scope.UnionFind = function(elementsArray) {
+        this.elements = elementsArray;
+        this.trackingArray = this.createTrackingArray();
+    };
 
-	scope.UnionFind.prototype = {
+    scope.UnionFind.prototype = {
+        createTrackingArray: function() {
+            var array = [];
+            for (var i = 0; i < this.elements.length; i++) {
+                array.push(i);
+            }
+            return array;
+        },
 
-	};
+        union: function(p, q) {
+            // if (this.connected(p, q)) return;
+            // var c = this.trackingArray[p];
+            // for (var i = 0; i < this.trackingArray.length; i++) {
+            //     if (this.trackingArray[i] === c) {
+            //         this.trackingArray[i] = this.trackingArray[q];
+            //     }
+            // }
+            var pid = this.trackingArray[p],
+            	qid = this.trackingArray[q];
+
+            for (var i = 0; i < this.trackingArray.length; i++) {
+            	if (this.trackingArray[i] === pid) {
+            		this.trackingArray[i] = qid;
+            	}
+            }
+
+        },
+
+        connected: function(p, q) {
+            return this.trackingArray[p] === this.trackingArray[q];
+        }
+    };
 })(this);
