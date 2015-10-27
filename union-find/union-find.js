17201836045510671d27e3afa99b7892f048487d
Kanstantsin_Vaitovich <Kanstantsin_Vaitovich@epam.com>
2015-10-27 15:47:11 +0300

    lazy implementation of union find

--- union-find/union-find.js
+++ union-find/union-find.js
@@ -15,27 +15,36 @@
             return array;
         },
 
-        union: function(p, q) {
-            // if (this.connected(p, q)) return;
-            // var c = this.trackingArray[p];
-            // for (var i = 0; i < this.trackingArray.length; i++) {
-            //     if (this.trackingArray[i] === c) {
-            //         this.trackingArray[i] = this.trackingArray[q];
-            //     }
-            // }
-            var pid = this.trackingArray[p],
-            	qid = this.trackingArray[q];
-
-            for (var i = 0; i < this.trackingArray.length; i++) {
-            	if (this.trackingArray[i] === pid) {
-            		this.trackingArray[i] = qid;
-            	}
+        root: function(i) {
+            while (i !== this.trackingArray[i]) {
+                i = this.trackingArray[i];
             }
+            return i;
+        },
 
+        union: function(p, q) {
+            // // if (this.connected(p, q)) return;
+            // // var c = this.trackingArray[p];
+            // // for (var i = 0; i < this.trackingArray.length; i++) {
+            // //     if (this.trackingArray[i] === c) {
+            // //         this.trackingArray[i] = this.trackingArray[q];
+            // //     }
+            // // }
+            // var pid = this.trackingArray[p],
+            // 	qid = this.trackingArray[q];
+
+            // for (var i = 0; i < this.trackingArray.length; i++) {
+            // 	if (this.trackingArray[i] === pid) {
+            // 		this.trackingArray[i] = qid;
+            // 	}
+            // }
+            var i = this.root(p),
+            	j = this.root(q);
+            this.trackingArray[i] = j;
         },
 
         connected: function(p, q) {
-            return this.trackingArray[p] === this.trackingArray[q];
+            return this.root(p) === this.root(q);
         }
     };
 })(this);
